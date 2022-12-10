7d423310530116f086037dda057059809407221e
Jason Chan <shunsei.chan@gmail.com>
2022-12-10 12:36:35 -0800

    Enemy Spawner System
    Still in development

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Enemy _enemy;
+    //Rotation Enemy should be facing when going straight relative to the spawner
+    [SerializeField] private float _straightAngle;
+    //This will be plus minus variation from striaght angle
+    [SerializeField] private float _angleVariationRange;
+    [SerializeField] private float _spawnRate = 1;
+    private float _timeSinceLastSpawned = 0;
+
+    private void Awake()
     {
-        
+        _timeSinceLastSpawned = _spawnRate;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_enemy == null) return;
+
+        _timeSinceLastSpawned += Time.deltaTime;
+        if (_timeSinceLastSpawned >= _spawnRate)
+        {
+            Transform enemyTransform = Instantiate(_enemy, transform.position, Quaternion.identity).transform;
+            enemyTransform.localEulerAngles = new Vector3(0, 0, _straightAngle + Random.Range(-_angleVariationRange, _angleVariationRange));
+            enemyTransform.parent = transform;
+            _timeSinceLastSpawned = 0;
+        }
     }
 }
