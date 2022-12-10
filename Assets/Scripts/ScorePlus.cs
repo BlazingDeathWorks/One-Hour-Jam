f013e36ab21cf36d4f641b48c3aa66e07fc90ba6
Century178 <mrothenberger0918@gmail.com>
2022-12-10 16:48:05 -0700

    Score Plus Spawner

--- Assets/Scripts/ScorePlus.cs
+++ Assets/Scripts/ScorePlus.cs
@@ -6,8 +6,18 @@ public class ScorePlus : MonoBehaviour
 {
     public int plusPower;
 
+    public float lifeSpan;
+
+    void Update()
+    {
+        Destroy(gameObject, lifeSpan);
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/ScorePlusSpawner.cs
+++ Assets/Scripts/ScorePlusSpawner.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class ScorePlusSpawner : MonoBehaviour
 {
-    [SerializeField] ScorePlus[] scorePlus;
+    [SerializeField] private ScorePlus[] scorePlus;
+
+    [SerializeField] private Vector2 spawnRange;
+
+    private float spawnTime;
+    [SerializeField] private float spawnSpeed;
 
     void Start()
     {
-        
+        spawnTime = spawnSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spawnTime -= Time.deltaTime;
+
+        if (spawnTime <= 0)
+        {
+            Instantiate(scorePlus[Random.Range(0, scorePlus.Length)], new Vector3(Random.Range(-spawnRange.x, spawnRange.x), Random.Range(-spawnRange.y, spawnRange.y), 0), transform.rotation);
+
+            spawnTime = spawnSpeed;
+        }
     }
 }
