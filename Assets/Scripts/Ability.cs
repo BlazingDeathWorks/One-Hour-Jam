6751c531b5e19f1f3590bc7c0ec250f15769c1b6
Jason Chan <shunsei.chan@gmail.com>
2022-12-09 19:52:00 -0800

    Ability Update

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ability : MonoBehaviour
+public abstract class Ability : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Ability"))
+        {
+            AbilityManager.Instance.Powerup = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void Unleash();
 }
--- Assets/Scripts/AbilityManager.cs
+++ Assets/Scripts/AbilityManager.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class AbilityManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static AbilityManager Instance { get; private set; }
+    public Ability Powerup { private get; set; } = null;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Powerup == null) return;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Powerup.Unleash();
+        }
     }
 }
