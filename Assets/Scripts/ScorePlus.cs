665c8cdbd13cb5751fd1b5059f3a6d1f3fc30b3a
Century178 <mrothenberger0918@gmail.com>
2022-12-10 15:48:13 -0700

    Organization and Variation

--- Assets/Scripts/ScorePlus.cs
+++ Assets/Scripts/ScorePlus.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class ScorePlus : MonoBehaviour
 {
     public int plusPower;
-
     public float lifeSpan;
+    public float speed;
 
     void Update()
     {
+        transform.Translate(transform.up * speed * Time.deltaTime);
+
         Destroy(gameObject, lifeSpan);
     }
 
