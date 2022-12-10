0ba622cb43fb6a71fde7ce1423a288cc592cff21
Century178 <mrothenberger0918@gmail.com>
2022-12-10 15:55:32 -0700

    Adjustments

--- Assets/Scripts/ScorePlus.cs
+++ Assets/Scripts/ScorePlus.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 public class ScorePlus : MonoBehaviour
 {
     public int plusPower;
-    public float lifeSpan;
     public float speed;
 
     void Update()
     {
         transform.Translate(transform.up * speed * Time.deltaTime);
-
-        Destroy(gameObject, lifeSpan);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
