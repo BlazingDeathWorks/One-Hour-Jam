4ed57876730d5ee04d1492000db689292919c38c
Jason Chan <shunsei.chan@gmail.com>
2022-12-10 20:38:49 -0800

    Scene control stuff

--- Assets/Scripts/SceneControl.cs
+++ Assets/Scripts/SceneControl.cs
@@ -5,9 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class SceneControl : MonoBehaviour
 {
+    public static SceneControl Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        Destroy(gameObject);
+    }
+
     public void GoToGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 
     public void GameQuit()
